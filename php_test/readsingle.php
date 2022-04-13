a8ee433c217a200d891409a2f23b18a4ebf5891d
mushfiqurgalib <48786723+mushfiqurgalib@users.noreply.github.com>
2022-04-13 20:46:21 +0600

    Add files via upload

--- php_test/readsingle.php
+++ php_test/readsingle.php
@@ -1,19 +1,45 @@
 <?php
 header('Access-Control-Allow-Origin:*');
 header('Content-type:application/json');
-header('Access-Control-Allow-Methods:GET');
+header('Access-Control-Allow-Methods:POST');
 header('Access-Control-Allow-Headers: Content-Type,Access-Control-Allow-Headers,Authorization,X-Request-With');
 
 
 $data=json_decode(file_get_contents("php://input"));
 include('db.php');
-$sql = "SELECT * FROM registration";
 
-    if(isset($_GET['id'])){
-        $sql="SELECT * FROM registration WHERE id=".$_GET['id'];
-    }
-
+$url = "http://66.45.237.70/api.php";
     
-    $run=mysqli_query($conn,$sql);
-    $products=mysqli_fetch_all($run,MYSQLI_ASSOC);
-    echo json_encode($products);
+        $sql="SELECT * FROM registration WHERE id='$data->id' AND name='$data->name' ";
+    
+        $result1=$conn->query($sql);
+    
+        if ($result1 ->num_rows > 0) {
+            // output data of each row
+            while($row = $result1 -> fetch_assoc()) {
+              $number= $row['mobile'];
+              $token=$row['token'];
+              
+
+              $text="Your one time pass is".'$token'."!";
+$data= array(
+'username'=>"G4L18",
+'password'=>"59FZRDXM",
+'number'=>"$number",
+'message'=>"$text"
+);
+
+
+$ch = curl_init(); // Initialize cURL
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$smsresult = curl_exec($ch);
+$p = explode("|",$smsresult);
+$sendstatus = $p[0];
+echo $sendstatus;
+
+            }
+          } else {
+            echo "wrong";
+          }
