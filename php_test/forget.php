e314db9f519a9a76d8006b3276741cfbb84486af
mushfiqurgalib <48786723+mushfiqurgalib@users.noreply.github.com>
2022-04-19 02:35:01 +0600

    Add files via upload

---
+++ php_test/forget.php
@@ -0,0 +1,51 @@
+<?php
+header('Access-Control-Allow-Origin:*');
+header('Content-type:application/json');
+header('Access-Control-Allow-Methods:POST');
+header('Access-Control-Allow-Headers: Content-Type,Access-Control-Allow-Headers,Authorization,X-Request-With');
+session_start();
+
+$data=json_decode(file_get_contents("php://input"));
+include('db.php');
+
+$url = "http://66.45.237.70/api.php";
+$token=rand(1000,9999);
+echo $token;
+$sql1="UPDATE registration SET token='$token' WHERE id='$data->id'  ";
+$result2=$conn->query($sql1);
+    
+        $sql="SELECT * FROM registration WHERE id='$data->id'  ";
+    
+        $result1=$conn->query($sql);
+    
+        if ($result1 ->num_rows > 0) {
+            // output data of each row
+            while($row = $result1 -> fetch_assoc()) {
+              $number= $row['mobile'];
+              $token=$row['token'];
+              
+              
+              
+              $text="Your Sultan tea garden OTP is ".$row['token']."!";
+              echo $text;
+$data= array(
+'username'=>"G4L18",
+'password'=>"59FZRDXM",
+'number'=>"$number",
+'message'=>"$text"
+);
+
+
+$ch = curl_init(); // Initialize cURL
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$smsresult = curl_exec($ch);
+$p = explode("|",$smsresult);
+$sendstatus = $p[0];
+echo $sendstatus;
+
+            }
+          } else {
+            echo "wrong";
+          }
