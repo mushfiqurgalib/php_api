4925c59d66e52331d2f45616927a9c02bf8a9e78
mushfiqurgalib <48786723+mushfiqurgalib@users.noreply.github.com>
2022-05-29 15:59:09 +0600

    Add files via upload

--- php_test/attendance.php
+++ php_test/attendance.php
@@ -37,21 +37,31 @@ header('Access-Control-Allow-Headers: Content-Type,Access-Control-Allow-Headers,
 $data=json_decode(file_get_contents("php://input"));
 include('db.php');
 
-
-  $sql = "SELECT COUNT(*) as 'att' FROM attendance where employeeid='$data->employeeid' AND status='1' AND MONTH(date)='$data->month' ";
+  $transactionid= rand(200000,299999);
+  $sql = "SELECT COUNT(*) as 'att' FROM attendance where employeeid='$_GET[id]' AND status='1' AND MONTH(date)='$_GET[month]' ";
   $result=$conn->query($sql);
   $html="";
   $html.="<h1 align='center'> Payslip  </h1>";
-   $sql1="SELECT id FROM users where id='$data->employeeid' ";
+  $sql1="SELECT id FROM users where id='$_GET[id]' ";
   $result1=$conn->query($sql1);
   $row1=mysqli_fetch_assoc($result1);
   $html.="<div ><p align='left'> Employee ID:".$row1['id'];
-  $html.="</p>";
- 
-  $html.="<p> Month:";
-  $html.=date("F", strtotime(date("Y") ."-". $data->month ."-01"));
-  $html.="</p> </div>";
   
+  $sql2="SELECT name FROM users WHERE id='$_GET[id]' ";
+  $result2=$conn->query($sql2);
+  $row2=mysqli_fetch_assoc($result2);
+ 
+   $html.="&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Month:".date("F", strtotime(date("Y") ."-". $_GET['month'] ."-01"));
+   
+  $html.="</p>";
+  $html.="<p> Name:".$row2['name'];
+ $html.="</p> ";
+ $html.="<p> Transaction ID: ";
+ $html.=$transactionid;
+ $row = mysqli_fetch_assoc($result); 
+   
+ $html.="<p> Attendance :" .$row['att'];
+ 
   $html.=
   "<table border='1' width='300' cellspacing='0'>
 
@@ -60,16 +70,11 @@ include('db.php');
 <th>Id</th>
 
 <th>name</th> </tr>";
-
+$html.=
+ "</td>
+ </table>";
       // output data of each row
-      $row = mysqli_fetch_assoc($result); 
-    
-$html.=
-"<tr>
-<td> " .$row['att'];
-$html.=
-"</td>
-</table>";
+   
 
 
         // // $html.=  "<td>" .$row['att'] "</td>"
