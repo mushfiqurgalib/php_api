dbae140bb648f831dbe18c2c4008e38ef2c215e3
mushfiqurgalib <48786723+mushfiqurgalib@users.noreply.github.com>
2022-05-29 12:01:06 +0600

    Add files via upload

--- php_test/fileslogic.php
+++ php_test/fileslogic.php
@@ -16,8 +16,11 @@ include('db.php');
 // Uploads files
  // if save button on the form is clicked
     // name of the uploaded file
+    $eid= $_POST['employeeid'];
+    $ename=$_POST['ename'];
+    $mobile=$_POST['mobile'];
     $filename = $_FILES['myfile']['name'];
-
+   
     // destination of the file on the server
     $destination = 'uploads/' . $filename;
 
@@ -27,7 +30,7 @@ include('db.php');
     // the physical file on a temporary uploads directory on the server
     $file = $_FILES['myfile']['tmp_name'];
     $size = $_FILES['myfile']['size'];
-
+    
     if (!in_array($extension, ['zip', 'pdf', 'docx'])) {
         echo "You file extension must be .zip, .pdf or .docx";
     } elseif ($_FILES['myfile']['size'] > 1000000) { // file shouldn't be larger than 1Megabyte
@@ -35,7 +38,7 @@ include('db.php');
     } else {
         // move the uploaded (temporary) file to the specified destination
         if (move_uploaded_file($file, $destination)) {
-            $sql = "INSERT INTO file (name, size, downloads) VALUES ('$filename', $size, 0)";
+            $sql = "INSERT INTO file (name, size, downloads,employeeid,ename,mobile) VALUES ('$filename', $size, 0,'$eid','$ename','$mobile')";
             if (mysqli_query($conn, $sql)) {
                 echo "File uploaded successfully";
             }
