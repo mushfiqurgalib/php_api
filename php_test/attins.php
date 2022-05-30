71bbeb75de1e12473afdb25fa7d18b292ae99e7e
mushfiqurgalib <48786723+mushfiqurgalib@users.noreply.github.com>
2022-05-30 23:58:24 +0600

    Update attins.php

--- php_test/attins.php
+++ php_test/attins.php
@@ -10,16 +10,21 @@ $data=json_decode(file_get_contents("php://input"));
 include('db.php');
 
 // Create connection
-
+$employeeid=$data->employeeid;
+$date=$data->date;
+$status=$data->status;
  
 // $date = date('Y-m-d', strtotime('date1'));
 
 //  $date = date('Y-m-d', strtotime('$data->date'));
- $sql = "INSERT INTO attendance (employeeid,date,status) VALUES ('$data->employeeid','$data->date','$data->status')";  
-
+if($employeeid){
+ $sql = "INSERT INTO attendance (employeeid,date,status) VALUES ('$employeeid','$date','$status')";  
+}
 $result=$conn->query($sql);
 
-if ($result) {
-    echo json_encode(['status' => 'success','msg'=>'added!']);
-  } else {
-    echo json_encode(['status' => 'failed','msg'=>'sorry!']);}
+if($result){
+  $response[] = array('status'=>1);
+}else{
+  $response[] = array('status'=>0);
+}
+echo json_encode($response);
