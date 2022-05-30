ef1bf303072085048ae65138b2b02c516988315a
mushfiqurgalib <48786723+mushfiqurgalib@users.noreply.github.com>
2022-05-30 15:42:13 +0600

    Create fileapptest.php

---
+++ php_test/fileapptest.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+
+header('Access-Control-Allow-Origin:*');
+header('Content-type:application/json');
+header('Access-Control-Allow-Methods:POST');
+header('Access-Control-Allow-Headers: Content-Type,Access-Control-Allow-Headers,Authorization,X-Request-With');
+
+
+$data=json_decode(file_get_contents("php://input"));
+include('db.php');
+// $employeeid=$_POST['id'];
+// $ename=$_POST['name'];
+// $mobile=$_POST['mobile'];
+$sql="INSERT INTO loan(employeeid,ename,mobile,amount) VALUES ('$_POST[id]','$_POST[name]','$_POST[mobile]','$_POST[amount]') ";
+if (mysqli_query($conn, $sql)) {
+    echo "File uploaded successfully";
+}
+ else {
+echo "Failed to upload file.";
+}
