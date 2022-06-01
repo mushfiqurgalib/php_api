b5fe744a5fbe35172267a213e33b9be66c24caa9
mushfiqurgalib <48786723+mushfiqurgalib@users.noreply.github.com>
2022-06-01 18:23:41 +0600

    Add files via upload

---
+++ php_test/farmerdelete.php
@@ -0,0 +1,22 @@
+<?php
+header('Access-Control-Allow-Origin:*');
+header('Content-type:application/json');
+header('Access-Control-Allow-Methods:POST');
+header('Access-Control-Allow-Headers: Content-Type,Access-Control-Allow-Headers,Authorization,X-Request-With');
+
+
+$data=json_decode(file_get_contents("php://input"));
+include('db.php');
+
+
+
+    
+
+$query="DELETE FROM loan  WHERE id='$_GET[id]' ";
+$result=mysqli_query($conn,$query);
+if($result){
+    $response[] = array('status'=>1);
+  }else{
+    $response[] = array('status'=>0);
+  }
+  echo json_encode($response);
