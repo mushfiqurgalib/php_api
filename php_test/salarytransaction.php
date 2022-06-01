156d57fd9859538358fa399c6801f2bf1335ca5f
mushfiqurgalib <48786723+mushfiqurgalib@users.noreply.github.com>
2022-06-01 14:29:11 +0600

    Add files via upload

---
+++ php_test/salarytransaction.php
@@ -0,0 +1,29 @@
+<?php
+
+
+
+
+header('Access-Control-Allow-Origin:*');
+header('Content-type:application/json');
+header('Access-Control-Allow-Methods:POST');
+header('Access-Control-Allow-Headers: Content-Type,Access-Control-Allow-Headers,Authorization,X-Request-With');
+
+
+$data=json_decode(file_get_contents("php://input"));
+include('db.php');
+include('attendancetransaction.php');
+// $employeeid=$_POST['id'];
+// $ename=$_POST['name'];
+// $mobile=$_POST['mobile'];
+
+$employeeid='$data->id';
+
+
+if($transactionid){
+$sql8="INSERT INTO transaction(tid,date,employeeid,amount,type) VALUES ('$transactionid',CURRENT_TIME(),'$_GET[id]','$net','salary') ";
+}if (mysqli_query($conn, $sql8)) {
+    echo "File uploaded successfully";
+}
+ else {
+echo "Failed to upload file.";
+}
