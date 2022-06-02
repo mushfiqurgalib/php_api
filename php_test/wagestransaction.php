4e41bebc2890c1ba3d756a61434b7bc9ce3ef426
mushfiqurgalib <48786723+mushfiqurgalib@users.noreply.github.com>
2022-06-02 13:09:36 +0600

    Add files via upload

---
+++ php_test/wagestransaction.php
@@ -0,0 +1,32 @@
+<?php
+
+
+
+
+header('Access-Control-Allow-Origin:*');
+header('Content-type:application/json');
+header('Access-Control-Allow-Methods:POST');
+header('Access-Control-Allow-Headers: Content-Type,Access-Control-Allow-Headers,Authorization,X-Request-With');
+
+
+$data=json_decode(file_get_contents("php://input"));
+include('db.php');
+
+// $employeeid=$_POST['id'];
+// $ename=$_POST['name'];
+// $mobile=$_POST['mobile'];
+$transactionid=rand(200000,299999);
+$employeeid='$data->id';
+$month='$data->month';
+$sql="SELECT COUNT(*) as 'att' FROM attendance where employeeid='$data->id' AND status='1' AND MONTH(date)='$data->month' ";
+$result=$conn->query($sql);
+$row = mysqli_fetch_assoc($result);
+$wage=$row['att']*300;
+if($transactionid){
+$sql8="INSERT INTO transaction(tid,date,employeeid,amount,type) VALUES ('$transactionid',CURRENT_TIME(),'$data->id','$wage','wages') ";
+}if (mysqli_query($conn, $sql8)) {
+    echo "File uploaded successfully";
+}
+ else {
+echo "Failed to upload file.";
+}
