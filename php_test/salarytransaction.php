3f00989f0bf923a9cb9a07e3471c1fd07cf23ffc
mushfiqurgalib <48786723+mushfiqurgalib@users.noreply.github.com>
2022-06-04 21:35:58 +0600

    Add files via upload

--- php_test/salarytransaction.php
+++ php_test/salarytransaction.php
@@ -10,6 +10,7 @@ header('Access-Control-Allow-Headers: Content-Type,Access-Control-Allow-Headers,
 
 
 $data=json_decode(file_get_contents("php://input"));
+// $url = "http://66.45.237.70/api.php";
 include('db.php');
 include('attendancetransaction.php');
 // $employeeid=$_POST['id'];
@@ -18,12 +19,40 @@ include('attendancetransaction.php');
 
 // $employeeid='$data->id';
 
-
+$month=date("F", strtotime(date("Y") ."-". '$data->month' ."-01"));
 if('$data->tid'){
 $sql8="INSERT INTO transaction(tid,date,employeeid,amount,type) VALUES ('$data->tid',CURRENT_TIME(),'$data->employeeid','$net','salary') ";
 }if (mysqli_query($conn, $sql8)) {
-    echo "File uploaded successfully";
-}
+    $response[] = array('status'=>1);
+
+    $text="Your salary of $month has been sent successfully!";
+    echo $text;
+    $sql9="SELECT mobile FROM users WHERE id='$data->employeeid'";
+    $result1=$conn->query($sql9);
+    
+    if ($result1 ->num_rows > 0) {
+        // output data of each row
+        while($row = $result1 -> fetch_assoc()) {
+          $number= $row['mobile'];
+              // echo $text;
+                $data= array(
+                'username'=>"G4L18",
+                'password'=>"59FZRDXM",
+                'number'=>"$number",
+                'message'=>"$text"
+                );
+
+
+            $ch = curl_init(); // Initialize cURL
+            curl_setopt($ch, CURLOPT_URL,$url);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $smsresult = curl_exec($ch);
+            $p = explode("|",$smsresult);
+            $sendstatus = $p[0];
+            echo $sendstatus;
+    }}}
  else {
-echo "Failed to upload file.";
+    $response[] = array('status'=>0);
 }
+echo json_encode($response);
