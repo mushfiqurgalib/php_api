e068194abc98efeb1f5c229931e11f6aca6f8a4c
mushfiqurgalib <48786723+mushfiqurgalib@users.noreply.github.com>
2022-06-04 17:22:59 +0600

    Add files via upload

---
+++ php_test/deletefile.php
@@ -0,0 +1,24 @@
+<?php
+header('Access-Control-Allow-Origin:*');
+header('Content-type:application/json');
+header('Access-Control-Allow-Methods:DELETE');
+header('Access-Control-Allow-Headers: Content-Type,Access-Control-Allow-Headers,Authorization,X-Request-With');
+
+
+$data=json_decode(file_get_contents("php://input"));
+include('db.php');
+
+
+$id=$data->id;
+if($id)
+{
+    
+
+$query="DELETE FROM file  WHERE id='$id' ";}
+$result=mysqli_query($conn,$query);
+if($result){
+    $response[] = array('status'=>1);
+  }else{
+    $response[] = array('status'=>0);
+  }
+  echo json_encode($response);
